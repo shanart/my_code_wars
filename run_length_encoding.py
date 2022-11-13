44451bec516250935c761571ca3e81b94effec22
shanart <shanin.x@gmail.com>
2022-11-13 22:02:41 +0200

    formatting

--- run_length_encoding.py
+++ run_length_encoding.py
@@ -2,15 +2,18 @@
 https://www.codewars.com/kata/546dba39fa8da224e8000467/train/python
 """
 
+
 def run_length_encoding(s) -> list:
-    if not s: return []
+    if not s:
+        return []
     result = []
     for i, l in enumerate(s):
-        if l != s[i-1] or not result:
+        if l != s[i - 1] or not result:
             result.append([1, l])
         else:
             result[-1][0] += 1
     return result
+
 
 """
 But much clever implementation on codewars was by using intertools.groupby() with list comprehensions
@@ -25,9 +28,13 @@ wheel in simple situations
 
 
 if __name__ == "__main__":
-    print(run_length_encoding(''))
+    print(run_length_encoding(""))
     print(run_length_encoding("abc"))
     print(run_length_encoding("aaaaab"))
     print(run_length_encoding("hello world!"))
     print(run_length_encoding("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbb"))
-    print(run_length_encoding("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"))
+    print(
+        run_length_encoding(
+            "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW"
+        )
+    )
